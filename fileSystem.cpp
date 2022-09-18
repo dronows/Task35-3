58a079a50088765414678d7b88573042d3fdf63e
ANDREJ <and423@inbox.lv>
2022-09-18 20:49:53 +0200

    Добавьте файлы проекта.

---
+++ fileSystem.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <filesystem>
+#include <memory>
+
+int main()
+{
+  std::filesystem::path pt("G:\\TestMap");
+
+  auto recursiveGetFileNamesByExtension = [](std::filesystem::path pt, const std::string extension) {
+	std::vector<std::filesystem::path> fileName;
+	if (std::filesystem::exists(pt) && std::filesystem::is_directory(pt)) {
+	  for (const auto& p : std::filesystem::recursive_directory_iterator(pt)) {
+		if (std::filesystem::is_regular_file(p)) {
+		  if (p.path().extension().compare(extension)) {
+			//std::filesystem::path temp( p.path().filename()) ;
+			fileName.push_back(p.path().filename());
+		  }
+		}
+	  }
+	}
+	return std::make_unique<std::vector<std::filesystem::path>>(begin(fileName), end(fileName));
+  };
+  
+  std::unique_ptr<std::vector<std::filesystem::path>> p = recursiveGetFileNamesByExtension(pt, " .txt");
+  for (auto& el : *p) std::cout << el << " ";
+
+  
+  return 0;
+}
+
